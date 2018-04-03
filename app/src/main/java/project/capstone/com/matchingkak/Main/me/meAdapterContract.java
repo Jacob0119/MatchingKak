c76368e6e2c519886a27f621df450554ac1cc332
Jacob0119 <amco16@naver.com>
2018-04-03 17:26:26 +0900

    no message

---
+++ app/src/main/java/project/capstone/com/matchingkak/Main/me/meAdapterContract.java
@@ -0,0 +1,4 @@
+package project.capstone.com.matchingkak.Main.me;
+
+public interface meAdapterContract {
+}
---
+++ app/src/main/java/project/capstone/com/matchingkak/Main/me/presenter/mePresenter.java
@@ -0,0 +1,6 @@
+package project.capstone.com.matchingkak.Main.me.presenter;
+
+import project.capstone.com.matchingkak.Main.me.meContract;
+
+public class mePresenter implements meContract.Presenter {
+}
