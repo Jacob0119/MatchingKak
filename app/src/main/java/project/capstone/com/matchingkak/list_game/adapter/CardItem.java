d1be5b94bb10d66424d0d8b0d3b2f46803348f3d
Jacab0119 <amco16@naver.com>
2018-04-11 01:12:24 +0900

    no message

--- app/src/main/java/project/capstone/com/matchingkak/list_game/adapter/CardItem.java
+++ app/src/main/java/project/capstone/com/matchingkak/list_game/adapter/CardItem.java
@@ -1,24 +1,40 @@
 package project.capstone.com.matchingkak.list_game.adapter;
 
 
+import project.capstone.com.matchingkak.restAPI.APIUrl;
+
 public class CardItem {
 
-    private String mTextResource;
-    private String mTitleResource;
+    private String state;
+    private String title;
+    private String location;
+    private String date;
+    private String weather;
+    private String picture= APIUrl.API_BASE_URL+"images/team-log2.png";
 
     public CardItem(int title, int text) {
         //mTitleResource = title;
        // mTextResource = text;
     }
-    public CardItem(String title,String text){
-            mTextResource=title;
-            mTitleResource=text;
+    public CardItem(String title,String date){
+            this.title=title;
+            this.date=date;
     }
-    public String getText() {
-        return mTextResource;
+    public String getDate() {
+        return date;
+    }
+
+    public String getPicture(){
+        return this.picture;
     }
 
     public String getTitle() {
-        return mTitleResource;
+        return title;
     }
+
+    public String getState() {return state;}
+
+    public String getLocation(){return location;}
+
+    public String getWeather(){return weather;}
 }
--- app/src/main/java/project/capstone/com/matchingkak/list_game/adapter/CardPagerAdapter.java
+++ app/src/main/java/project/capstone/com/matchingkak/list_game/adapter/CardPagerAdapter.java
@@ -1,23 +1,32 @@
 package project.capstone.com.matchingkak.list_game.adapter;
 
+import android.databinding.BindingAdapter;
+import android.databinding.DataBindingUtil;
+import android.graphics.drawable.Drawable;
 import android.support.v4.view.PagerAdapter;
 import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import project.capstone.com.matchingkak.R;
+import project.capstone.com.matchingkak.databinding.ListGameCardviewBinding;
+
 
 public class CardPagerAdapter extends PagerAdapter implements CardAdapter {
 
     private List<CardView> mViews;
     private List<CardItem> mData;
     private float mBaseElevation;
-
+    // binding;
+    ListGameCardviewBinding binding;
     public CardPagerAdapter() {
         mData = new ArrayList<>();
         mViews = new ArrayList<>();
@@ -79,10 +88,21 @@ public class CardPagerAdapter extends PagerAdapter implements CardAdapter {
     }
 
     private void bind(CardItem item, View view) {
-        TextView titleTextView = view.findViewById(R.id.titleTextView);
-        TextView contentTextView = view.findViewById(R.id.contentTextView);
-        titleTextView.setText(item.getTitle());
-        contentTextView.setText(item.getText());
+         binding= DataBindingUtil.bind(view);
+         binding.setCarditem(item);
+        loadImage(binding.listGameViewPicture,item.getPicture(),view.getContext().getDrawable(R.drawable.main_logo));
+    }
+
+    @BindingAdapter({"imageUrl","error"})
+    public static void loadImage(ImageView imageView , String imageUrl, Drawable error){
+        RequestOptions requestOptions=new RequestOptions()
+                                        .centerCrop()
+                                         .circleCrop()
+                                        .error(error)
+                                        ;
+        Glide.with(imageView.getContext()).load(imageUrl).apply(requestOptions).into(imageView);
+
+
     }
 
 }
